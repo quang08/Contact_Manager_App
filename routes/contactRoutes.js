c357aff358bff600f54d02801c7c94563da32f45
quang <nguyenthequang080203@gmail.com>
2023-05-30 16:09:52 -0700

    refactor routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,14 +8,8 @@ const {
   deleteContact,
 } = require("../controllers/contactControllers");
 
-router.route("/").get(getContacts);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/:id").get(getContact);
-
-router.route("/").post(createContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
