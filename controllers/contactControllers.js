043b737d9a4f212587a706de554f002cb895468f
quang <nguyenthequang080203@gmail.com>
2023-05-31 13:10:05 -0700

    add create contact and get all contacts function

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,15 +1,18 @@
 //**** controllers handle/contain logic for routers ****
 
-//In Express, route handlers or middleware functions often contain asynchronous operations such as interacting with databases or making API calls. 
-//These asynchronous operations typically involve promises or async/await syntax. 
-//However, if an error occurs within an asynchronous function, Express does not handle it automatically. It's necessary to catch the error 
+//In Express, route handlers or middleware functions often contain asynchronous operations such as interacting with databases or making API calls.
+//These asynchronous operations typically involve promises or async/await syntax.
+//However, if an error occurs within an asynchronous function, Express does not handle it automatically. It's necessary to catch the error
 //and pass it to the next error-handling middleware or send an appropriate response.
-//The express-async-handler package provides a convenient way to wrap asynchronous functions 
+//The express-async-handler package provides a convenient way to wrap asynchronous functions
 //and automatically catch any errors within them. It eliminates the need for explicit try/catch blocks in each async function and simplifies error handling.
 const asyncHandler = require("express-async-handler");
 
+const Contact = require("../models/contactModel");
+
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 const getContact = asyncHandler(async (req, res) => {
@@ -18,14 +21,19 @@ const getContact = asyncHandler(async (req, res) => {
 
 //POST requests if succeeded, return 201: new resources created
 const createContact = asyncHandler(async (req, res) => {
-  const { name, email, number } = req.body;
+  const { name, email, phone } = req.body;
   //Error throwing: However this way the error is not in JSON but HTMl, so we should create a custom middleware
   //where it takes incoming req and res then convert errors to JSON if there's any
-  if (!name || !email || number) {
+  if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mandatory");
   }
-  res.status(201).json({ message: "Created a contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
 });
 
 const updateContact = asyncHandler(async (req, res) => {
