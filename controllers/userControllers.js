fa66d78333ffd964dd636a6d1d9888b6d8ab4526
quang <nguyenthequang080203@gmail.com>
2023-05-31 19:00:30 -0700

    add user login feature with jwt access token

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //we accept username, email and password to register an user.
 //However we cannot just store the password raw, we have to hash it before storing
@@ -37,6 +38,35 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
+  //receive info
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+
+  //retrieve user to compare
+  const user = await User.findOne({ email });
+
+  //compare passwords
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "2m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or Password not valid");
+  }
+
   res.json({ message: "Logged in an user" });
 });
 
